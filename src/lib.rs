762a13129926eaa89aec64c908e3b87ed805461d
Andrés Núñez <nunezlopez.ja@gmail.com>
2025-10-24 14:02:03 -0600

    A little more refactoring. I am barely understanding this

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,11 @@
-use std::{env, fs};
+
+pub mod site;
+pub mod content;
+
+pub use site::create_site;
+pub use content::create_content;
 
 pub fn check_valid_archetype(name: &str) -> bool {
     name.ends_with(".md")
 }
 
-// Creates a new site
-pub fn create_site(name: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let mut path = env::current_dir()?;
-    path.push(&name);
-    if path.exists() {
-        let err = format!("Site {} already exists!", &name);
-        return Err(err.into());
-    }
-    fs::create_dir_all(&path)?;
-    env::set_current_dir(&path)?;
-    fs::create_dir("content")?;
-    fs::create_dir("static")?;
-    fs::create_dir("layouts")?;
-    fs::create_dir("themes")?;
-    fs::create_dir("archetypes")?;
-    fs::create_dir("assets")?;
-    fs::create_dir("data")?;
-    fs::File::create("config.toml")?;
-
-    new_site_msg(&name);
-    Ok(())
-}
-
-fn new_site_msg(name: &str) {
-    println!("Congratulations! Your new site \"{}\", was created!", &name);
-    println!("\nJust a few more steps... \n");
-    println!("1. Change the current directory to {}/", &name);
-    println!(
-        "2. Create new content with the command \"rusite new content <SECTIONNAME>/<FILENAME>.<FORMAT>\"."
-    );
-    println!(
-        "3. Start the embedded web server with the command \"rusite server --buildDrafts\"."
-    );
-}
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,8 @@
-use std::{env, fs, process};
+use std::process;
 
 use clap::{Parser, Subcommand};
 
-use rusite::create_site;
-use rusite::check_valid_archetype;
-
-fn create_content(name: &str) -> Result<(), Box<dyn std::error::Error>> {
-    if !check_valid_archetype(&name) {
-        return Err("Error: Failed to resolve \"{name}\" to an archetype template".into());
-    }
-
-    let mut path = env::current_dir()?;
-
-    // Check if we're in an actual site directry
-    let mut toml_path = path.clone();
-    toml_path.push("config.toml");
-    if toml_path.exists() {
-        println!("We can kinda think we are inside an actual site directory. Creating post...")
-    } else {
-        return Err(
-            "Not inside a site. Have you changed directory to your site? Can't create content.".into(),
-        );
-    }
-
-    path.push("content");
-
-    // Check if directory (or content type) was provided and act accordingly
-    if name.contains("/") {
-        if let Some((content_dir, post_name)) = name.rsplit_once("/") {
-            path.push(content_dir);
-            if !path.exists() {
-                fs::create_dir(&path)?;
-            }
-            path.push(post_name);
-        }
-    } else {
-        if !path.exists() {
-            fs::create_dir(&path)?;
-        }
-        path.push(name);
-    }
-
-    fs::File::create(&path)?;
-    println!("Your new content \"{}\" was created!", &name);
-
-    Ok(())
-}
+use rusite::{site, content};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -84,8 +41,8 @@ fn run(args: Args) -> Result<(), Box<dyn std::error::Error>> {
     match args.cmd {
         Commands::New { target } => {
             match target {
-                NewTarget::Site { name } => create_site(&name)?,
-                NewTarget::Content { name } => create_content(&name)?,
+                NewTarget::Site { name } => site::create_site(&name)?,
+                NewTarget::Content { name } => content::create_content(&name)?,
             }
         }
     };
